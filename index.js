a0250c51058a17d1616a11bf15cae6ac24b3906e
maitri0411 <maitribaria96@gmail.com>
2019-05-15 15:03:38 +0530

    updated the pages

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function animateMe(elem) {
   }
 }*/
 
-var $animation_elements = $('.animation-element');
+/*var $animation_elements = $('.animation-element');
 var $window = $(window);
 
 function check_if_in_view() {
@@ -87,4 +87,126 @@ function check_if_in_view() {
 }
 
 $window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+$window.trigger('scroll');*/
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1000);
+
+            }
+
+        });
+
+    });
+
+});
+
+
+$(document).ready(function() {
+
+	$(window).scroll( function(){
+
+		// slide in from left to right
+		$('.slide-right').each(function() {
+			var objBot = $(this).offset().top + $(this).outerHeight();
+			var winBot = $(window).scrollTop() + $(window).height();
+
+			if( winBot > objBot ){
+				$(this).animate({
+					opacity: 1,
+					marginRight: "0",
+					marginLeft: "0"
+				}, 500, 'linear' );
+			}
+		});
+
+		// slide in from right to left
+		$('.slide-left').each(function() {
+			var objBot = $(this).offset().top + $(this).outerHeight();
+			var winBot = $(window).scrollTop() + $(window).height();
+
+			if( winBot > objBot ){
+				$(this).animate({
+					opacity: 1,
+					marginRight: "0",
+					marginLeft: "0"
+				}, 500, 'linear' );
+			}
+		});
+
+		// fade in when obj is 33% in viewport
+		$('.fade-in').each( function(){
+			var objBot = $(this).offset().top + ($(this).outerHeight() / 3);
+			var winBot = $(window).scrollTop() + $(window).height();
+
+			if( winBot > objBot ){
+				$(this).animate({
+					opacity: 1
+				}, 800 );
+			}
+		});
+
+		// slide in from right to left (always visible ) when obj is 50% in viewport
+		$('.visible-slide-left').each(function() {
+			var objBot = $(this).offset().top + ($(this).outerHeight() / 2);
+			var winBot = $(window).scrollTop() + $(window).height();
+
+			if( winBot > objBot ){
+				$(this).animate({
+					marginRight: "-40px",
+					marginLeft: "40px"
+				}, 600, 'linear' );
+			}
+		});
+
+		// slide in from left to right (always visible ) when obj is 50% in viewport
+		$('.visible-slide-right').each(function() {
+			var objBot = $(this).offset().top + ($(this).outerHeight() / 2);
+			var winBot = $(window).scrollTop() + $(window).height();
+
+			if( winBot > objBot ){
+				$(this).animate({
+					marginRight: "40px",
+					marginLeft: "-40px"
+				}, 600, 'linear' );
+			}
+		});
+
+	});
+
+});
+
+$(function() {
+  var $elements = $('.animateBlock.notAnimated'); //contains all elements of nonAnimated class
+  var $window = $(window);
+  $window.on('scroll', function(e) {
+    $elements.each(function(i, elem) { //loop through each element
+      if ($(this).hasClass('animated')) // check if already animated
+        return;
+      animateMe($(this));
+    });
+  });
+});
+
+function animateMe(elem) {
+  var winTop = $(window).scrollTop(); // calculate distance from top of window
+  var winBottom = winTop + $(window).height();
+  var elemTop = $(elem).offset().top; // element distance from top of page
+  var elemBottom = elemTop + $(elem).height();
+  if ((elemBottom <= winBottom) && (elemTop >= winTop)) {
+    // exchange classes if element visible
+    $(elem).removeClass('notAnimated').addClass('animated');
+  }
+}
